2c904a734abfc69bbd8a9bac54da963cdee5cefd
2912600742 <107752473+2912600742@users.noreply.github.com>
2023-07-01 15:10:26 +0800

    Update and rename README.md to fcousclock.py

---
+++ fcousclock.py
@@ -0,0 +1,14 @@
+import time
+
+def focus_timer(minutes):
+    seconds = minutes * 60
+    while seconds:
+        mins, secs = divmod(seconds, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds -= 1
+    print("时间到！")
+
+# 设置专注时长为25分钟（可根据需要自行调整）
+focus_timer(25)
